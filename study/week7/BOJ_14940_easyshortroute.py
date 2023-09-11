e04398b7de821a57a6daa482049f15d6f3c63764
Chico <silverstuff@naver.com>
2023-09-12 08:28:13 +0900

    0912asa

--- study/week7/BOJ_14940_easyshortroute.py
+++ study/week7/BOJ_14940_easyshortroute.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from collections import deque
 
 dy = [-1, 0, 1, 0]
@@ -8,7 +7,6 @@ def BFS():
     global cnt
     while q:
         a = q.popleft()
-        
         for k in range(4):
             ny = a[0]+dy[k]
             nx = a[1]+dx[k]
@@ -17,18 +15,20 @@ def BFS():
                     visited[ny][nx]  = visited[a[0]][a[1]] +1
                     now = ny,nx
                     q.append(now)
-                    # print(visited)
-        # cnt += 1
+
 
 N, M = map(int, input().split())
-
 arr = [list(map(int, input().split()))for _ in range(N)]
 visited = [[-1]*(M) for _ in range(N)]
-# cnt = 1
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 0:
+            visited[i][j] = 0
 
 for i in range(N):
         for j in range(M):
             if arr[i][j] == 2:
+                visited[i][j] = 0
                 q = deque()
                 start = i,j
                 q.append(start)
