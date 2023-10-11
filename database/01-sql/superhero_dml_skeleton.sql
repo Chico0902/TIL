15137063c7cecb6dcab0fa8cb60a672a4bc85632
chico <silverstuff@naver.com>
2023-10-11 17:08:54 +0900

    1011gogo

--- database/01-sql/superhero_dml_skeleton.sql
+++ database/01-sql/superhero_dml_skeleton.sql
@@ -93,50 +93,127 @@ WHERE 국적 = '미국' OR 국적 = '러시아';
 -- %(percent): 글자 수 제한 없이 패턴을 만족하면 조회
 -- 3.6이름이 ~~맨 으로 끝나는 사람들 조회
 
+SELECT *
+FROM superhero
+WHERE 이름 LIKE '%맨';
+
+-- 이름 '아' 로 싲가해서, 맨으로 끝나는 데이터 조회
+
+SELECT *
+FROM superhero
+WHERE 이름 LIKE '아%맨';
+
 -- _(underscore): 개수 만큼 글자 수 제한하여 패턴을 만족하면 조회
 -- 3.7 이름이 두 글자인 사람들만 조회
 
+SELECT *
+FROM superhero
+WHERE 이름 LIKE '__';
+
 -- 특정 데이터에 포함여부(IN)
 -- 3.8 마블, DC 소속의 사람들을 조회
+
+SELECT * 
+FROM superhero
+WHERE 소속회사 = '마블' OR 소속회사 =  'DC';
+
+-- 조금 더 직관적으로 사용할 수 있다.
+
+SELECT * 
+FROM superhero
+WHERE 소속회사 IN ('마블', 'DC');
+
+-- 포함되지 않았을 때(NOT IN)도 사용가능
+
+SELECT * 
+FROM superhero
+WHERE 소속회사 NOT IN ('마블', 'DC');
 
 ----------------------
 
 -- 특정 조건 사이에 존재하는 데이터 조회(BETWEEN ... AND ... )
 
 -- 3.9 나이가 100~500살 사이의 사람들을 조회
+SELECT * 
+FROM superhero
+WHERE 나이 BETWEEN 100 AND 500;
 
 ---------------------- 4. 집계함수
 
 -- 원하는 행 개수만큼만 조회(LIMIT)
 
+
 -- 4.1 나이가 가장 적은 사람 1명
+
+SELECT *
+FROM superhero
+ORDER BY 나이 LIMIT 1;
 
 -- 4.2 소속회사가 마블인 사람 중 나이가 가장 적은 1명
 
+SELECT *
+FROM superhero
+WHERE 소속회사 = 'DC'
+ORDER BY 나이 LIMIT 1;
+
 -- 4.3 나이가 많은 10명
+SELECT *
+FROM superhero
+ORDER BY 나이 DESC 
+LIMIT 10;
 
 -- N 번째 데이터부터 조회 - 기준점을 변경(OFFSET)
 -- 4.4 나이가 10번째로 많은 사람
 
--- 4.5 나이가 10번째 ~ 15번째 사람들
+SELECT *
+FROM superhero
+ORDER BY 나이 DESC 
+LIMIT 1
+OFFSET 9;
+
+-- 4.5 나이가 10번째 ~ 14번째 사람들
+SELECT *
+FROM superhero
+ORDER BY 나이 DESC 
+LIMIT 5
+OFFSET 9;
 
 ----------------------
 
 -- 데이터 수 구하기(COUNT)
 
 -- 4.6 전체 데이터 수를 구하여라
-
+SELECT COUNT(*)
+FROM superhero;
 -- 조건문과 함께 활용하기
 -- 4.7 직업이 악당인 사람의 수
+SELECT COUNT(*) AS 악당수
+FROM superhero
+WHERE 직업 = '악당';
 
 -- 평균 구하기(AVG)
 
--- 4.8 마블의 영웅들의 평균 나이를 구하여라.
+-- 4.8 전체 평균 나이를 구하여라.
+SELECT AVG(나이) AS 평균나이
+FROM superhero;
+-- 마블소속만 구하려면
+-- WHERE 소속회사 = '마블';
 
 -- 그룹 별 계산(GROUP BY)
 -- 4.9 각 소속회사의 평균 나이를 구하여라
+SELECT 소속회사, AVG(나이) AS 평균나이
+FROM superhero
+GROUP BY 소속회사;
 
 -- 4.10 소속회사 별로 그룹화 하여, 40살 이상인 데이터만 조회하여라.
+SELECT 소속회사, AVG(나이) AS 평균나이
+FROM superhero
+WHERE 나이 >= 40
+GROUP BY 소속회사;
 
 -- 그룹화 후에 조건주기(HAVING)
--- 4.11 각 소속회사 별 40세 이상인 사람들의 평균 나이를 구하여라
+-- 4.11 평균 나이가 40살 이상인 소속회사를 구하여라.
+SELECT 소속회사, AVG(나이) AS 평균나이
+FROM superhero
+GROUP BY 소속회사
+HAVING AVG(나이) >= 40;
---
+++ database/02-01-many-to-one-relationships/articles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    # 부모 객체가 삭제되었을 때 이를 참조하는 객체도 삭제(on_delete 의 CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
