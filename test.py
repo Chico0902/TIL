538315bdffa011eb9ddfc8affb71d791deddd95d
chico <silverstuff@naver.com>
2023-10-17 18:15:37 +0900

    1017gogo

--- test.py
+++ test.py
@@ -1,12 +1,29 @@
-N = int(input())
+import sys
+sys.setrecursionlimit(10**6)
+N, M ,R = map(int, input().split())
+arr = [[] for _ in range(N+1)]
+visited = [0]*(N+1)
+result = []
 cnt = 0
-for i in range(1,N+1):
-    for k in str(i):
-        for j in ['3','6','9']:
-            if j in k:
-                cnt +=1
-    if not cnt:
-        print(i,end=' ')
-    else:
-        print('-'*cnt,end=' ')
-    cnt = 0
+
+def DFS(start):
+    global cnt
+    now = start
+    cnt += 1
+    visited[now] = cnt
+    for i in range(N):
+        if i in arr[now] and not visited[i]:
+            visited[i] = 1
+            DFS(i)
+
+
+
+for i in range(N):
+    a, b = map(int,sys.stdin.readline().split())
+    arr[a].append(b)
+    arr[b].append(a)
+
+
+DFS(R)
+print(*visited[1:],sep='\n')
+
