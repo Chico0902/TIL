17c40a482be7df461a395d8876f6163a0bd1252a
chico <silverstuff@naver.com>
2023-10-18 18:05:54 +0900

    1018gogo

--- Algo/Algo_Code/BOJ/BOJ_24479.py
+++ Algo/Algo_Code/BOJ/BOJ_24479.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**4)
 N, M ,R = map(int, input().split())
 arr = [[] for _ in range(N+1)]
 visited = [0]*(N+1)
@@ -11,19 +11,23 @@ def DFS(start):
     now = start
     cnt += 1
     visited[now] = cnt
-    for i in range(N):
+    for i in arr[now]:
         if i in arr[now] and not visited[i]:
             visited[i] = 1
             DFS(i)
 
 
 
-for i in range(N):
+for i in range(M):
     a, b = map(int,sys.stdin.readline().split())
     arr[a].append(b)
     arr[b].append(a)
 
 
+for i in range(N):
+    arr[i].sort()
+
 DFS(R)
+
 print(*visited[1:],sep='\n')
 
