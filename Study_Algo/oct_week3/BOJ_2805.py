f062182e02aff90baf6eb6b8a5d17fb10f78e039
Chico <silverstuff@naver.com>
2023-10-24 02:04:24 +0900

    1023night

--- Study_Algo/oct_week3/BOJ_2805.py
+++ Study_Algo/oct_week3/BOJ_2805.py
@@ -1,3 +1,45 @@
-N, M = map(int, input().split())
+import sys
+input = sys.stdin.readline
 
-arr = list
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort(reverse=True)
+sumTree = 0
+checkhigh = 0
+checkpoint = 0
+ans = 0
+newsumTree = 0
+while sumTree < M:
+    for i in range(1,N):
+        cutTree = arr[i-1] - arr[i]
+        sumTree += cutTree*i
+        if sumTree == M:
+            ans = arr[i]
+            newsumTree = M
+            break
+        elif sumTree > M:
+            checkhigh = arr[i-1]
+            checkpoint = i
+            newsumTree = sumTree - cutTree*i
+            break
+    break
+print(checkhigh)
+print(checkpoint)
+print(newsumTree)
+while newsumTree < M:
+    for i in range(1,M):
+        newcut = checkpoint*i
+        newsumTree += newcut
+        if newsumTree > M:
+            ans = checkhigh-(i+1)
+            break
+
+print(ans)
+
+
+
+
+
+
+
+
