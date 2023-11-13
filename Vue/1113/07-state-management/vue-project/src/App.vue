744c68f97365ba32867a457de8622578a961b5da
Chico <silverstuff@naver.com>
2023-11-13 18:00:18 +0900

    1113gogo

---
+++ Vue/1113/07-state-management/vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script setup>
+  import { sayT1, sayDongHyeon } from './utils/donghyun.js'
+
+  sayT1()
+  sayDongHyeon()
+</script>
+
+<style scoped>
+
+</style>
