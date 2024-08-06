1e13b0cf692ba92784da12413f9644fded20fb6c
chico <silverstuff@naver.com>
2024-08-06 21:40:50 +0900

    08.06

---
+++ Algo/CHiCO/24.08/08.06/BOJ_9372.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+T =  int(input())
+for _ in range(T):
+    plane = {}
+    N, M = map(int, input().split())
+    for a in range(M):
+        a,b = map(int, input().split())
+    print(N-1)
