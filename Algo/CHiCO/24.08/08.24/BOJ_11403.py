33ea8fdafd5726dc2619c1b51cbbf9781cd77ece
chico <silverstuff@naver.com>
2024-08-24 21:00:52 +0900

    08.24

--- Algo/CHiCO/24.08/08.24/BOJ_11403.py
+++ Algo/CHiCO/24.08/08.24/BOJ_11403.py
@@ -27,3 +27,27 @@ for start in range(N):
 
 for row in visited:
     print(" ".join(map(str, row)))
+
+
+# 플로이드 워셜이라는 알고리즘
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+
+# 인접 행렬 입력 받기
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
+
+# 플로이드-워셜 알고리즘 적용
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if arr[i][k] and arr[k][j]:
+                arr[i][j] = 1
+
+# 결과 출력
+for row in arr:
+    print(" ".join(map(str, row)))
