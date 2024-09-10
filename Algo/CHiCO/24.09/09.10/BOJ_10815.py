4ab82b0cdaa821c0917c462bc53479e9ee0feeba
chico <silverstuff@naver.com>
2024-09-10 13:18:50 +0900

    09.10

---
+++ Algo/CHiCO/24.09/09.10/BOJ_10815.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+card = set(map(int,input().split()))
+M = int(input())
+comp = list(map(int, input().split()))
+ans = []
+print(card)
+for i in comp:
+    if i in card:
+        ans.append(1)
+    else:
+        ans.append(0)
+
+print(*ans)
